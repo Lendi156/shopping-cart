a253197f737216c252d13c6af97355a139d53fb5
lendi156 <lendi156@gmail.com>
2021-11-04 20:05:36 +0700

    Create Product Component using card component from material ui.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
 import { Container, Stack } from '@mui/material'
+import Product from './component/Product'
 
 function App () {
   return (
     <Container>
       <Stack spacing={2}>
-
+        <Product />
       </Stack>
     </Container>
   )
---
+++ src/component/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card, CardContent, Typography } from '@mui/material'
+
+export default function Product () {
+  return (
+        <Card>
+          <CardContent>
+            <Typography>
+              Test
+            </Typography>
+          </CardContent>
+        </Card>
+  )
+}
