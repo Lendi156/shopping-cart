f34fc905e6f8cbf8463f387d10aac3fe93f27da3
lendi156 <lendi156@gmail.com>
2021-11-04 19:40:42 +0700

    Adding container and grid component

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import { Container, Grid } from '@mui/material'
+
+function App () {
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item>
+
+        </Grid>
+        <Grid item>
+
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default App
