8292f3cbc133604a60a368c9e71ca7b451cb753f
lendi156 <lendi156@gmail.com>
2021-11-05 13:35:10 +0700

    Add card componet to empty card message

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Button, Stack, Typography } from '@mui/material'
+import { Container, Button, Stack, Typography, Card, CardContent } from '@mui/material'
 import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { onBuy } from '../utils/utils'
@@ -21,9 +21,17 @@ export default function CheckOut () {
       <Stack spacing={2} sx={{ margin: '80px 0 20px' }}>
         {cartItems.length > 0
           ? <>
-                {cartListComponent}
+              {cartListComponent}
             </>
-          : <Typography>Tidak ada barang</Typography> }
+          : <>
+            <Card sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <CardContent>
+                <Typography>
+                  Tidak ada barang di cart
+                </Typography>
+              </CardContent>
+            </Card>
+          </> }
         <Stack direction='row' justifyContent='flex-end' spacing={2}>
           <Button variant="contained" onClick={() => navigate('/')}>Back to Home</Button>
           {cartItems.length > 0 ? <Button variant="contained" onClick={() => onBuy(products, dispatch, navigate)}>Buy Products</Button> : null }
